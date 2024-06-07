18ae66e7fb71370cca7c0be0936725b44fe4c286
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-06-07 15:03:02 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15";
+
+function listaImagenes() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((datosImagenes) => {
+      console.log(datosImagenes);
+
+      const card = document.querySelector("[data-url]");
+
+      datosImagenes.forEach((element) => {
+        const contenido = `<li class="card">
+      <img class="card__image" src="${element.url}" alt="imagen">
+      <h3 class="card__title">${element.title}</h3>
+  </li>`;
+
+        card.innerHTML = card.innerHTML + contenido;
+
+        console.log(card.innerHTML);
+      });
+    })
+    .catch((error) => console.log(error));
+}
+
+listaImagenes();
