a0ffa07c9b47870349fcb677db0d32207514e137
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-06-11 13:35:13 -0600

    Fix: change then/catch for async await

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15";
+const url =
+  "https://api.nasa.gov/planetary/apod?api_key=itSbr9bhOj0TUkk1sDTzPddvSS8aVpGTsce1HFm4&count=15";
 
-function listaImagenes() {
+//Async await
+async function listaImagenes() {
+  try {
+    let fetchImagen = await fetch(url);
+    let datosImagenes = await fetchImagen.json();
+
+    console.log("datos imagenes:", datosImagenes);
+
+    const card = document.querySelector("[data-ul]");
+
+    datosImagenes.forEach((element) => {
+      const contenido = `<li class="card">
+        <img class="card__image" src="${element.url}" alt="imagen">
+        <h3 class="card__title">${element.title}</h3>
+        </li>`;
+
+      card.innerHTML = card.innerHTML + contenido;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Then y catch
+/*function listaImagenes() {
   fetch(url)
     .then((response) => response.json())
     .then((datosImagenes) => {
@@ -10,16 +35,15 @@ function listaImagenes() {
 
       datosImagenes.forEach((element) => {
         const contenido = `<li class="card">
-      <img class="card__image" src="${element.url}" alt="imagen">
-      <h3 class="card__title">${element.title}</h3>
-  </li>`;
+        <img class="card__image" src="${element.url}" alt="imagen">
+        <h3 class="card__title">${element.title}</h3>
+        </li>`;
 
         card.innerHTML = card.innerHTML + contenido;
 
-        console.log(card.innerHTML);
       });
     })
     .catch((error) => console.log(error));
 }
-
+*/
 listaImagenes();
